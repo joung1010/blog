0cd6b2053899ea67c5ec0579f83a0810e4e4f59a
mason park <joung4342@gmail.com>
2023-10-30 18:37:51 +0900

    featuredPost 작업2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Profile from "@/components/profile/Profile";
 import {getFeaturedPosts} from "@/service/posts/posts";
+import Image from "next/image";
 export default async function Home() {
     const featuredPost = await getFeaturedPosts();
   return (
@@ -8,9 +9,13 @@ export default async function Home() {
           {/*서버 컴퍼넌트로 생성, 클라이언트 컴퍼넌트 + API 형식으로 생성*/}
           <ul>
               {
-                  featuredPost.map(({title,description,path},index) => (
+                  featuredPost.map(({title,description,path,category,date},index) => (
                       <li key={index}>
+                          <Image src={`/images/posts/${path}.png`} alt={title} width='300' height='300'/>
+                          {date}
                           {title}
+                          {description}
+                          {category}
                       </li>
                   ))
               }
