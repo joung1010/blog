7f29a0ca6e43bba19b5e342902f729cb1aae01fe
mason park <joung4342@gmail.com>
2023-11-02 19:20:44 +0900

    react-multi-carousel 작업2

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -2,6 +2,5 @@ import {getFeaturedPosts, getPosts} from "@/service/posts/posts";
 
 export async function GET(request:Request) {
     const posts = await getPosts();
-    const userAgent = request.headers.get('user-agent');
-    return Response.json({posts,userAgent});
+    return Response.json(posts);
 }
--- src/components/posts/slider/PostSlider.tsx
+++ src/components/posts/slider/PostSlider.tsx
@@ -4,6 +4,7 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import {Post} from "@/service/posts/posts";
 import MobileDetect from "mobile-detect";
+import Image from "next/image";
 
 const responsive = {
     superLargeDesktop: {
@@ -24,7 +25,7 @@ const responsive = {
         items: 1
     }
 };
-type Device = 'tablet' | 'mobile' | 'desktop'|string;
+
 
 function getDevice():Device {
     const md = new MobileDetect(navigator.userAgent);
@@ -50,8 +51,10 @@ function PostSlider() {
             });
     },[]);
     return (
-        <>
+        <div className='my-6'>
+            <h2 className="ml-4 text-2xl font-bold">You may like</h2>
             {!posts && <div>Loading...</div>}
+            {posts &&
             <Carousel responsive={responsive}
                       ssr
                       deviceType={device}
@@ -59,13 +62,25 @@ function PostSlider() {
                       containerClass='carousel-container'
                       itemClass='carousel-image-item'
             >
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
-              <div>1</div>
+                {
+                    posts.map(({path,title,date,description,category},index)=>(
+                        <div key={index} className='rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105 m-5'>
+                            <Image src={`/images/posts/${path}.png`} alt={title} width='300' height='300'  className="w-full h-2/3"/>
+                            <section className="flex flex-col items-center justify-center">
+                                <div className="m-2 mr-4 self-end">{date}</div>
+                                <h2 className="text-xl font-bold">{title}</h2>
+                                <p>{description}</p>
+                                <div className="bg-green-300 px-2 py-1 rounded-xl my-2">{category}</div>
+                            </section>
+                        </div>
+                    ))
+                }
             </Carousel>
-        </>
+            }
+        </div>
     );
 }
+
+type Device = 'tablet' | 'mobile' | 'desktop'|string;
 
 export default PostSlider;
