d4fbc50840d0b0cbcabc9ec30a89e4c2469c41c5
mason park <joung4342@gmail.com>
2023-11-07 20:42:36 +0900

    Contact 작업(Form 다루기2)

--- src/components/contract/ContactForm.tsx
+++ src/components/contract/ContactForm.tsx
@@ -1,29 +1,36 @@
 'use client';
-import React, {FormEvent} from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
 
 
 function ContactForm() {
+    const [send, setSend] = useState<Send>({email:'',subject:'', message: ''});
+    const onHandleChange = (e:ChangeEvent<HTMLInputElement|HTMLTextAreaElement>) => {
+        const {name, value} = e.target;
+        setSend((send) => ({...send, [name]: value}));
+    };
 
     const onSubmit = (e:FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const formData = new FormData(e.currentTarget);
-        console.log(formData);
-
+        console.log(send);
     };
 
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <label htmlFor="email">You Email</label>
-                <input type="text" inputMode="email" id="email" />
+                <input type="text" name="email" autoComplete="email"  placeholder="Email Address" onChange={onHandleChange}/>
                 <label htmlFor="subject">Subject</label>
-                <input type="text" inputMode="text" id="subject" />
+                <input type="text" name="subject" placeholder="Subject" autoComplete="subject" onChange={onHandleChange}/>
                 <label htmlFor="message">Message</label>
-                <textarea id="message" rows={5}/>
+                <textarea rows={5} name="message" autoComplete="message" placeholder="Message" onChange={onHandleChange}/>
                 <button type="submit">Submit</button>
             </form>
         </div>
     );
 }
-
+type Send = {
+    email: string;
+    subject: string;
+    message: string;
+}
 export default ContactForm;
