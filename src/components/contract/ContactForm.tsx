a3adef14aed229d2b8f1fee6c41aa75d2221c12e
mason park <joung4342@gmail.com>
2023-11-08 19:22:56 +0900

    Contact 작업(validation yup)3

--- src/components/contract/ContactForm.tsx
+++ src/components/contract/ContactForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, {ChangeEvent, FormEvent, useState} from 'react';
-
+import * as yup from 'yup';
 
 function ContactForm() {
     const [send, setSend] = useState<Send>({email:'',subject:'', message: ''});
@@ -9,20 +9,35 @@ function ContactForm() {
         setSend((send) => ({...send, [name]: value}));
     };
 
-    const onSubmit = (e:FormEvent<HTMLFormElement>) => {
+    const onSubmit = async (e:FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log(send);
+        const isValid = await schema.isValid(send);
+        if (!isValid) {
+            alert('필수 입력 사항을 모두 입력해주세요.');
+            return;
+        }
+        fetch('/api/send/mail',{
+            method:'POST',
+            headers:{
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(send),
+        })
+            .then(response => response.json())
+            .then(data => {console.log(data)})
+            .catch(error => console.error(error));
+
     };
 
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <label htmlFor="email">You Email</label>
-                <input type="text" name="email" autoComplete="email"  placeholder="Email Address" onChange={onHandleChange}/>
+                <input type="text" name="email" autoComplete="email" required  placeholder="Email Address" onChange={onHandleChange}/>
                 <label htmlFor="subject">Subject</label>
-                <input type="text" name="subject" placeholder="Subject" autoComplete="subject" onChange={onHandleChange}/>
+                <input type="text" name="subject" placeholder="Subject" required autoComplete="subject" onChange={onHandleChange}/>
                 <label htmlFor="message">Message</label>
-                <textarea rows={5} name="message" autoComplete="message" placeholder="Message" onChange={onHandleChange}/>
+                <textarea rows={5} name="message" autoComplete="message" required placeholder="Message" onChange={onHandleChange}/>
                 <button type="submit">Submit</button>
             </form>
         </div>
@@ -33,4 +48,9 @@ type Send = {
     subject: string;
     message: string;
 }
+const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    subject: yup.string().required(),
+    message: yup.string().required(),
+});
 export default ContactForm;
