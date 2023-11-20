ba4167ed86bc7479401a788fe4b8838b4ae53df9
mason park <joung4342@gmail.com>
2023-11-20 20:18:39 +0900

    소스 원복

--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -3,21 +3,16 @@ import React, {useEffect, useState} from 'react';
 import {Post} from "@/service/posts/posts";
 import PostCard from "@/components/postCard/PostCard";
 
-type Props = {
-    filter: boolean;
-}
-function Posts({filter}:Props) {
+function Posts({title,url}:{title:string,url:string}) {
     const [featuredPost,setFeaturedPost] = useState<Post[] | undefined>();
     useEffect(() => {
-        fetch('/api/posts')
+        fetch(url)
             .then(res => res.json())
-            .then(data => {
-                const posts:Post[] = [...data].filter(post =>)
-            });
+            .then(data => {setFeaturedPost(data)});
     },[]);
     return (
-        <>
-
+        <section>
+            <h2 className="ml-4 text-2xl font-bold">{title}</h2>
             <ul className="grid grid-col-1 md:grid-col-2 lg:grid-cols-3 gap-4 p-4">
                 {
                     featuredPost?.map((post, index) => (
@@ -25,7 +20,7 @@ function Posts({filter}:Props) {
                     ))
                 }
             </ul>
-        </>
+        </section>
     );
 }
 
