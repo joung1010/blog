7ebbb929b9044f3850247a43f627158cc5e2e0a3
mason park <joung4342@gmail.com>
2023-12-05 19:45:43 +0900

    소스코드 정리

--- src/app/api/posts/nav/route.ts
+++
@@ -1,6 +0,0 @@
-import {Category, getPostsCategories} from "@/service/posts/posts";
-
-export async function GET(request: Request) {
-    const categories: Category[] = await getPostsCategories();
-    return Response.json(categories);
-}
