eaf74d9bbcd174ff7523c326258edceccfad779f
mason park <joung4342@gmail.com>
2024-01-18 19:49:46 +0900

    Markdown 스타일링

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {getPostData} from "@/service/posts/posts";
 import Markdown from "react-markdown";
 import MarkDownViewer from "@/components/common/markdown/MarkDownViewer";
+import Image from "next/image";
+import {AiTwotoneCalendar} from "react-icons/ai";
 
 
 type Props = {
@@ -11,13 +13,23 @@ type Props = {
 };
 
 async function PostDetailPage({params:{slug}}:Props) {
-    const post = await getPostData(slug);
+    const {content,title,path,description,date} = await getPostData(slug);
     return (
-        <div>
-            {/*테일윈드를 사용하면 기본적으로 CSS가 초기화 된다.*/}
-            <h1>{post.title}</h1>
-            <MarkDownViewer content={post.content}/>
-        </div>
+        <article className='rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4'>
+            <Image className='w-full h-1/5 max-h-[500px]'
+                src={`/images/posts/${path}.png`}
+                alt={title} width={760} height={420}/>
+            <section className='flex flex-col p-4'>
+                <div className='flex items-center self-end text-sky-600'>
+                    <AiTwotoneCalendar/>
+                    <p className='font-semibold ml-2'>{date.toString()}</p>
+                </div>
+                <h1 className='text-4xl font-bold'>{title}</h1>
+                <p className='text-xl font-bold'>{description}</p>
+                <div className='w-44 border-2 border-sky-500 mt-4 mb-8'></div>
+                <MarkDownViewer content={content}/>
+            </section>
+        </article>
     );
 }
 
